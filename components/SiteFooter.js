aeb4f50c1fa2d6b8ec22214f8410f0fba0db5d33
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2017-04-03 11:37:48 -0700

    hardlink atom feed

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () =>
   <footer className='SiteFooter'>
-    <a href='/atom.xml'>
+    <a href='https://emilyaviva.github.io/gatsby-starter-blog-multiple-authors/atom.xml'>
       <i className='fa fa-rss' aria-hidden='true' />{' '}Atom Feed
     </a>
   </footer>
